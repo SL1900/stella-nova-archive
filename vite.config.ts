89e472cf3944879b54bec7563bda471b058f39b2
BB-69 <129858054+BB-69@users.noreply.github.com>
2025-11-23 15:21:49 +0700

    Change name

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/nova-archive/',
+  plugins: [react(), tailwindcss()],
+  base: "/stella-nova-archive/",
   server: {
-    host: false
-  }
-})
+    host: false,
+  },
+});
