200ee7a5738c1779156981551b6fbe4bd987cdc9
BB-69 <129858054+BB-69@users.noreply.github.com>
2025-11-30 20:18:22 +0700

    Added tag labels

---
+++ src/scripts/structs/tag-data.ts
@@ -0,0 +1,51 @@
+const tags = [
+  {
+    level: "primary",
+    tag: ["illustration", "live2d", "battle_stage", "ui", "other"],
+  },
+  {
+    level: "secondary",
+    tag: ["background", "character", "disc", "ascension", "encyclopedia"],
+  },
+  {
+    level: "tertiary",
+    tag: ["element", "rarity", "domain"],
+  },
+];
+
+function getTagLevel(tag: string): string {
+  for (const tl of tags) {
+    if (tl.tag.includes(tag)) return tl.level;
+  }
+  return "none";
+}
+
+const tagColor: Record<string, { col1: string; col2: string }> = {
+  // index category
+  primary: {
+    col1: "#660066",
+    col2: "#FFCCFF",
+  },
+  // sub category
+  secondary: {
+    col1: "#006600",
+    col2: "#CCFFCC",
+  },
+  // exclusive type
+  tertiary: {
+    col1: "#006666",
+    col2: "#CCFFFF",
+  },
+  // default
+  none: {
+    col1: "#555555",
+    col2: "#EEEEEE",
+  },
+};
+
+export function getTagColor(tag: string): {
+  col1: string;
+  col2: string;
+} {
+  return tagColor[getTagLevel(tag)];
+}
