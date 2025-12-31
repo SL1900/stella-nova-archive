84258a4ea1ab58b4bf56e8fba4aafdb2acbfe9f8
BB-69 <129858054+BB-69@users.noreply.github.com>
2025-12-31 13:00:55 +0700

    Add ButtonInput

--- src/components/layout/page-archive/TranslationBar/useTlOptions.tsx
+++ src/components/layout/page-archive/TranslationBar/useTlOptions.tsx
@@ -22,6 +22,7 @@ import {
 } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { getImageDimensions } from "../../../../scripts/image";
+import ButtonInput from "../../../common/button-input";
 
 export interface TlOptionProps {
   id: string;
@@ -116,35 +117,18 @@ const useTlOptions = ({
       appearOn: { md: true, edit: true },
       alwaysShowContentOnFull: true,
       content: (
-        <div
-          className="group-unselectable p-[4px] my-1 w-full max-h-full
-          flex justify-center items-start"
-        >
-          <div
-            className="group relative flex justify-center items-center
-            max-w-full max-h-full rounded-xl border-1
-            border-black/50 [.dark_&]:border-white/50
-            hover:border-white [.dark_&]:hover:border-black
-            hover:bg-[var(--bg-a1)] [.dark_&]:hover:bg-white
-            hover:text-white [.dark_&]:hover:text-[var(--bg-a1-dark)]
-            pl-2 pr-3 text-sm font-bold whitespace-nowrap
-            transition duration-100"
-          >
+        <ButtonInput
+          label="Upload Image"
+          icon={<Upload />}
+          htmlInput={
             <input
               className="absolute inset-0 opacity-0"
               type="file"
               accept="image/*"
               onChange={handleImageChange}
             />
-            <span
-              className="flex flex-row items-center py-2
-              h-full gap-2 opacity-70 group-hover:opacity-100"
-            >
-              <Upload />
-              <span className="pb-[1.7px]">Upload Image</span>
-            </span>
-          </div>
-        </div>
+          }
+        />
       ),
     },
     {
