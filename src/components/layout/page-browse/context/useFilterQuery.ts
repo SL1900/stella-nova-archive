2fa72a0a62f0d60fda839ada51282d20188c9c5a
BB-69 <129858054+BB-69@users.noreply.github.com>
2026-01-03 11:07:35 +0700

    TEMP_RENAME - skibidi-context -> context

--- src/components/layout/page-browse/context/useFilterQuery.ts
+++
@@ -1,7 +0,0 @@
-import { useFilterContext } from "./FilterContext";
-
-export function useFilterQuery() {
-  const { filterQuery, setFilterQuery } = useFilterContext();
-
-  return { query: filterQuery, setQuery: setFilterQuery };
-}
